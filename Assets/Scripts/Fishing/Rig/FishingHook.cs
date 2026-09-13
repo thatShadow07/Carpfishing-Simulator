94d8a83160d9cecb8a2a022233b7a6d1b07ed9b3
thatShadow07 <darkwall1705@gmail.com>
2026-09-13 02:09:47 +0100

    Script para fishing hook

--- Assets/Scripts/Fishing/Rig/FishingHook.cs
+++ Assets/Scripts/Fishing/Rig/FishingHook.cs
@@ -1,16 +1,59 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class FishingHook : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Bait")]
+    [SerializeField] private Transform bait;
+
+    [Header("Hook State")]
+    public bool IsHooked { get; private set; }
+    public Transform HookedFish { get; private set; }
+
+    [Header("Rig")]
+    [SerializeField, Min(0.01f)] private float hooklinkLength = 0.20f;
+
+    public Transform Bait => bait;
+    public float HooklinkLength => hooklinkLength;
+
+    private Rigidbody rb;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
+        // O Hook não se move sozinho.
+        // A física da luta será adicionada quando estiver ligado ao Hooklink.
+        rb.isKinematic = true;
+        rb.useGravity = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetBait(Transform newBait)
     {
-        
+        bait = newBait;
+    }
+
+    public void HookFish(Transform fish)
+    {
+        if (fish == null || IsHooked)
+            return;
+
+        IsHooked = true;
+        HookedFish = fish;
+
+        Debug.Log($"FishingHook: peixe fisgado -> {fish.name}");
+    }
+
+    public void UnhookFish()
+    {
+        IsHooked = false;
+        HookedFish = null;
+
+        Debug.Log("FishingHook: peixe desengatado.");
+    }
+
+    public bool HasBait()
+    {
+        return bait != null;
     }
 }
